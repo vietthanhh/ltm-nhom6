d4d7c95213f72732ed8234bc89f3ae89d52ad1ef
ngoctran <tthngoc113@gmail.com>
2026-08-11 11:01:21 +0700

    Update constants.py

--- Code/common/constants.py
+++ Code/common/constants.py
@@ -1,3 +1,8 @@
+"""
+Hằng số dùng chung cho toàn bộ app (client + server)
+KHÔNG tự đổi giá trị ở đây khi đang code module riêng — mọi thay đổi
+phải được nhóm thống nhất trước, vì ảnh hưởng tới tất cả module khác.
+"""
 SERVER_PORT = 5000
 CHUNK_SIZE = 4096
 MAX_CONCURRENT = 5
