3ade2ed6d06f174b43eb79a4fe82bbc0f53fbab4
ngoctran <tthngoc113@gmail.com>
2026-08-11 10:56:31 +0700

    Update task.py

--- Code/common/task.py
+++ Code/common/task.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 @dataclass
-class Task:
+class task:
     task_id: str
     filename: str          # tên gốc ở server
     final_filename: str    # tên sau khi xử lý trùng, VD: report(1).pdf
